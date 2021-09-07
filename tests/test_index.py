f2a0dc2a67319d45703fdc3d7e51861ccbc96374
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-07 17:08:29 +1000

    Add test

---
+++ tests/test_index.py
@@ -0,0 +1,11 @@
+import pytest
+from bs4 import BeautifulSoup
+
+@pytest.fixture
+def index(client):
+    rv = client.get('/')
+    return BeautifulSoup(rv.data, 'html.parser')
+
+
+def test_title(index):
+    assert index.select('h2')[0].string == 'COVID Futures'
