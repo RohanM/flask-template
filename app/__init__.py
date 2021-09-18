cdd7a6a75a99f0c79c9351e9c0170cc5d988844c
Rohan Mitchell <rohan@rohanmitchell.com>
2021-09-18 15:49:43 +1000

    Configure database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 import os
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(test_config=None):
-    # create and configure the app
+    # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
+    if test_config:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/dev.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Database
+    db.init_app(app)
+
+    # Blueprints
     from . import index
     app.register_blueprint(index.bp)
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
 import pytest
 
-from app import create_app
+from app import create_app, db
 
 @pytest.fixture
-def app(): return create_app()
+def app(): return create_app(True)
+
+@pytest.fixture
+def database(app):
+    db.create_all()
+    return db
